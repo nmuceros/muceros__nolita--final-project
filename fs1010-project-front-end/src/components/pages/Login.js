c506190726e7769bb19d18a56525913296af4ab0
nmuceros <nolita.muceros@gmail.com>
2021-10-11 17:51:47 -0400

    adds login page

--- fs1010-project-front-end/src/components/pages/Login.js
+++ fs1010-project-front-end/src/components/pages/Login.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react'
 import { Container, Col, Row, Button, Form, FormGroup, Label, Input, Card, CardBody, CardText } from 'reactstrap'
 import { useHistory, useLocation } from 'react-router-dom'
+import { BsFillPersonFill } from "react-icons/bs"
+import { RiLockPasswordFill } from "react-icons/ri"
+import "../../css/login.css"
 
 const Login = () => {
     let history = useHistory();
@@ -33,30 +36,96 @@ const Login = () => {
 
     return (
         <Container>
-        {!auth && 
-            <Card className="text-white bg-primary my-5 py-4 text-center">
-            <CardBody>
-                <CardText className="text-white m-0">Invalid credentials, please try again</CardText>
-            </CardBody>
-        </Card>
-        }
-        <Form className="my-5" onSubmit={loginSubmit}>
-          <Row form>
-            <Col md={6}>
-              <FormGroup>
-                <Label for="usernameEntry">Username</Label>
-                <Input type="text" name="username" id="usernameEntry" placeholder="Username" value={username} onChange={e => setUsername(e.target.value)}/>
-              </FormGroup>
-            </Col>
-            <Col md={6}>
-              <FormGroup>
-                <Label for="passwordEntry">Password</Label>
-                <Input type="password" name="password" id="passwordEntry" placeholder="Valid password" onChange={e => setPassword(e.target.value)}/>
-              </FormGroup>
-            </Col>
-          </Row>
-          <Button color="success">Sign in</Button>
-        </Form></Container>
+          <main className="login-container">
+            {!auth && 
+                <Card className="text-white bg-primary my-5 py-4 text-center">
+                <CardBody>
+                    <CardText className="text-white m-0">Invalid credentials, please try again</CardText>
+                </CardBody>
+            </Card>
+            }
+
+            <h2 className="loginTitle-container">
+                    <div className="loginTitle-texts">For Authorized User Only!</div>
+            </h2>                
+
+             <section className="loginForm-container">            
+                <Form className="my-5" onSubmit={loginSubmit}>
+                  {/* <Row form>
+                      <Col md={6}> */}
+
+                        {/* <FormGroup>
+                          <Label for="usernameEntry">Username</Label>
+                          <Input type="text" name="username" id="usernameEntry" placeholder="Username" value={username} onChange={e => setUsername(e.target.value)}/>
+                        </FormGroup> */}
+
+                        <FormGroup row>
+                            <Col xs={3} sm={2} md={2} lg={1}>                    
+                                <div className="userIDIcon-container"> 
+                                        <BsFillPersonFill />         
+                                </div>                          
+                            </Col>
+                            <Col xs={9} sm={10} md={10} lg={11}>
+                                <div className="inputUserID-containier">
+                                    <Input 
+                                        type="text" 
+                                        name="username" 
+                                        id="usernameEntry" 
+                                        placeholder="Username" 
+                                        value={username} 
+                                        onChange={e => setUsername(e.target.value)}
+                                    />
+                                </div>    
+                            </Col>
+                        </FormGroup>  
+
+                      {/* </Col> */}
+
+                      {/* <Col md={6}> */}
+
+                        {/* <FormGroup>
+                          <Label for="passwordEntry">Password</Label>
+                          <Input type="password" name="password" id="passwordEntry" placeholder="Valid password" onChange={e => setPassword(e.target.value)}/>
+                        </FormGroup> */}
+                        <FormGroup row>
+                            <Col xs={3} sm={2} md={2} lg={1}>                    
+                                <div className="userNameIcon-container"> 
+                                        <RiLockPasswordFill />         
+                                </div>                          
+                            </Col>
+                            <Col xs={9} sm={10} md={10} lg={11}>
+                                <div className="inputUserName-containier">
+                                    <Input 
+                                        type="password" 
+                                        name="password" 
+                                        id="passwordEntry" 
+                                        placeholder="Valid password" 
+                                        required value={username} 
+                                        onChange={e => setPassword(e.target.value)}
+                                    />
+                                </div>    
+                            </Col>
+                        </FormGroup>  
+
+
+                      {/* </Col> */}
+
+                  {/* </Row> */}
+                  {/* <Button color="success">Sign in</Button> */}
+
+                      <FormGroup>
+                          <Col>
+                              <div id="login-sendButton-container">
+                                  <Button id="login-signInButton" color="secondary" size="sm">Sign In</Button>
+                              </div>
+                          </Col>
+                      </FormGroup>
+
+
+                </Form>
+              </section>  
+          </main>
+        </Container>
     )
 }
 
