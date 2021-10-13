c9537889e8d5ad3ad6779d645ee425ba2165248d
nmuceros <nolita.muceros@gmail.com>
2021-10-13 02:45:53 -0400

    updates home page style

--- fs1010-project-front-end/src/components/pages/Home.js
+++ fs1010-project-front-end/src/components/pages/Home.js
@@ -17,40 +17,42 @@ const Home = () => {
     return (
         <Container>
             <main>
-                <section>
+                <section className="section">
                     <Row className="my-5">
                         <Col lg="5">
-                 
+                            <div className="leftSection-container">
                                  
-                            <Row>
-                                <div className="aboutMe-container">                                    
-                                    <div className="imageFrame">
-                                        <img id="myPhoto" src={profilePhoto} alt="profilePhoto" />
+                                <Row>
+                                    <div className="aboutMe-container">                                    
+                                        <div className="imageFrame">
+                                            <img id="myPhoto" src={profilePhoto} alt="profilePhoto" />
+                                        </div>
+                                                                
+                                     </div>
+                                </Row> 
+                                <Row>
+                                    <div className="homeQuote-container1">
+                                        <p className="texts-experts">"Experts once know basic. They became expert because someone like you have given them a chance.</p>
                                     </div>
-                                                               
-                               </div>
-                            </Row> 
-                            <Row>
-                              <div className="homeQuote-container">
-
-                                 
-                                    <p>"Experts started from the basic. They became expert because someone like you have given them a chance.</p>
-                                    <p>Add a new expert to your team... hire me!"</p>
-                                
-                               
-                              </div>
-                            </Row> 
+                                    <div className="homeQuote-container2">
+                                        <p className="texts-add">Add an expert to be... hire me!"</p>
+                                        
+                                    
+                                     </div>
+                                </Row> 
+                            </div>    
                         </Col>
                 
                         <Col lg="7">
-                            <h1 id="homeHello-texts" className="font-weight-light">hELLO!</h1>
-                            <h2 id="homeMyName-texts" className="font-weight-light">My name is <strong className="myName">Nolita Muceros</strong></h2>
-                            <h3 className="homeAspiring-texts">Aspiring Full-Stack Web Developer</h3>
-                            <p className="homeCurrently-texts">Currently a student of "Full-Stack Web Development" program at York University.  </p>
-                            {/* <p>Get to know me...</p> */}
+                            <div className="rightSection-container">
+                                <h1 id="homeHello-texts" className="font-weight-light">hELLO!</h1>
+                                <h2 id="homeMyName-texts" className="font-weight-light">My name is  <strong className="myName">Nolita Muceros</strong></h2>
+                                <h3 className="homeAspiring-texts">Aspiring Full-Stack Web Developer</h3>
+                                <p className="homeCurrently-texts">Currently expanding skills in "Full-Stack Web Development"</p>
+                                {/* <p>Get to know me...</p> */}
 
-                            <Button id="findOutMore-button" color="secondary" onClick = { buttonHandler}> Find out more... </Button>
-
+                                <Button id="findOutMore-button" color="secondary" onClick = { buttonHandler}> Recent projects... </Button>
+                            </div>
                         </Col>
                     </Row>
                 </section>
