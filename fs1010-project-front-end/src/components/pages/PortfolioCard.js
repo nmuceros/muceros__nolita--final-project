f0e47f8a1ff9a5be390ba15063a295919e0a01be
nmuceros <nolita.muceros@gmail.com>
2021-10-14 23:49:42 -0400

    replaces button on collapse and adds git links

--- fs1010-project-front-end/src/components/pages/PortfolioCard.js
+++ fs1010-project-front-end/src/components/pages/PortfolioCard.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react"
 import { Container, Card, CardBody, CardImg, CardSubtitle, CardTitle, CardText, CardFooter, Button, Collapse } from "reactstrap"
+import { CgDetailsMore } from "react-icons/cg"
+import { FaGitSquare } from "react-icons/fa"
 import "../../css/portfolioCard.css"
 
 
@@ -10,18 +12,6 @@ const PortfolioCard = (project) => {
     const [isOpen, setIsOpen] = useState(false);
     const toggle = () => setIsOpen(!isOpen);
 
-    // const handleMore = () => {
-    //     setSelectedProject({
-    //         projectID: projectObject.projectID,
-    //         projectName: projectObject.projectName,
-    //         projectImage: projectObject.projectImage,
-    //         projectbBuiltBy: projectObject.projectbBuiltBy,      
-    //         projectTitle: projectObject.projectTitle,     
-    //         projectDesc: projectObject.projectDesc
-    //     });
-    //     setIsOpen(!isOpen);
-    // }
-
     return (
         <Container>
             <Card id="portfolio-card-container" >
@@ -30,30 +20,22 @@ const PortfolioCard = (project) => {
                 </div>
                 <CardBody>
                     <CardTitle id="portfolio-cardTitle" tag="h4">{project.projectName}</CardTitle>
-
-
                     <div>
-                    <Button className="portfolio-collapse-button" color="secondary" onClick={toggle}  style={{ marginBottom: '1rem' }}>Details</Button>
+                        <div className="portfolio-card-links">
+                            <a className="portfolio-collapse-icon" onClick={toggle}>
+                                <CgDetailsMore />
+                            </a>
+                            <a className="portfolio-gitLink-icon">
+                                <FaGitSquare />
+                            </a>
+                        </div>
                         <Collapse className="portfolio-collapse" isOpen={isOpen}>
-                            {/* <Card> */}
-                                {/* <CardBody> */}
                                {project.projectDesc}
-                                {/* </CardBody> */}
-                                {/* </Card> */}
                         </Collapse>
                     </div>
 
                 </CardBody>
-
-                {/* <CardBody>
-                    <CardTitle id="portfolio-cardTitle" tag="h5">{project.projectName}</CardTitle>
-                    <CardSubtitle id="portfolio-cardSubTitle" ag="h6" className="mb-2 text-muted">{project.projectDesc}</CardSubtitle>  
-                    <CardText id="portfolio-cardText">{project.projectDesc}</CardText>
-                    <Button id="portfolio-collapse-button" color="secondary" size="sm">Button</Button>
-                </CardBody>        */}
-
             </Card>            
-
         </Container>
     )    
 }
